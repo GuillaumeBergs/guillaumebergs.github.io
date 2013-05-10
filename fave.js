82916568b61f0c78415b968b903a2faf49f9743d
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-10 14:28:57 +0200

    Update fave.js

--- fave.js
+++ fave.js
@@ -504,12 +504,12 @@ switch(fromus_site)	//Permet de sélectionner le code relatif au site consulté
 		case "www.juicycouture.com":
 			{
 				fromus_objectname			=	document.getElementsByClassName("product-name").innerText;		
-			
+		alert(	fromus_objectname);
 				fromus_pricemintmp				=	document.getElementsByClassName("product-price")[0].innerText;
 				fromus_pricemin					=	/(\$[0-9]{0,}\.[0-9]{2})/gi.exec(fromus_pricemintmp)[0];
-			
+		alert(	fromus_pricemin);
 				fromus_img							=	document.getElementsByClassName("product-primary-image")[0].getElementsByTagName("a")[0].href;				
-
+		alert(fromus_img);
 				fromus_desc							=	document.getElementsByClassName('cnt product-description')[0].innerText;
 			}break;
 			
