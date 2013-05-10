e9c5a7f0a72023a9ac50f5a18ce9a532c31632e1
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-10 13:56:43 +0200

    Update fave.js

--- fave.js
+++ fave.js
@@ -463,16 +463,14 @@ switch(fromus_site)	//Permet de sélectionner le code relatif au site consulté
 		case "www.jcrew.com":
 			{	
 				fromus_objectname				=	document.getElementById("pdp-title").innerText;		
-alert(fromus_objectname);
-				fromus_pricemintmp				=	document.getElementsByClassName("pdp-single")[0];
-				fromus_pricemin					=	fromus_pricemintmp.innerText + '';	
-				fromus_pricemin					=	/(\$[0-9]{0,}\.[0-9]{2})/g.exec(fromus_pricemin)[0];				
-alert(fromus_pricemin);
-				fromus_imgtmp					=	document.getElementsByClassName("prod_main_img")[0].innerHTML;
-				fromus_imgtmp					+=	'';				
+
+				fromus_pricemintmp				=	document.getElementsByClassName("pdp-single")[0].innerText;
+				fromus_pricemin					=	/(\$[0-9]{0,}\.[0-9]{2})/g.exec(fromus_pricemintmp)[0];				
+
+				fromus_imgtmp					=	document.getElementsByClassName("prod_main_img")[0].innerText;		
 				fromus_img						= 	/(http\:\/\/.*)(\?\$pdp)/gi.exec(fromus_imgtmp)[0] + '';
 				fromus_img						=	fromus_img.substring(0,fromus_img.length-5);	
-alert(fromus_img);
+
 				fromus_desc						=	document.getElementsByClassName('descmore_text')[1].innerText;
 			}break;
 		
