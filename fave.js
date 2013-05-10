ac447beafd4124974b33d6f3d829508d98c44212
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-10 14:40:33 +0300

    Update fave.js

--- fave.js
+++ fave.js
@@ -457,7 +457,9 @@ switch(fromus_site)	//Permet de sélectionner le code relatif au site consulté
 				fromus_pricemin					=	fromus_pricemintmp[0].innerText + '';	
 				fromus_pricemin					=	/(\$[0-9]{0,}\.[0-9]{2})/gi.exec(fromus_pricemin)[0];		
 				
-				fromus_img						=	document.getElementById("product_photo").src;
+				fromus_img							=	document.getElementById("product_photo").src;
+				
+				fromus_desc							= document.getElementsByClassName("so-product-description")[0].innerText;	
 
 				
 		}break;
