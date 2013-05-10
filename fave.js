fa8a52f3560ca8ca52fecfede123504f16c87b92
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-10 11:43:08 +0300

    Update fave.js

--- fave.js
+++ fave.js
@@ -1545,11 +1545,15 @@ switch(fromus_site)	//Permet de sélectionner le code relatif au site consulté
 if(fromus_desc==undefined)//En cas d'abscence de description, utiliser le nom du produit.
 		{
 			fromus_desc									=	fromus_objectname;
-		}	
+		}
+else
+		{
+			fromus_desc									=	fromus_desc.substring(0,200);
+		}
 	
 // stockage du nom dans local storage
 localStorage["regName"] = fromus_objectname;
 // stockage du prix dans local storage
 localStorage["regPrice"] = fromus_pricemin;		
 	
-window.alert("Vendeur: \n" + fromus_site + "\n\nOffre: \n" + fromus_offre + "\n\nNom: \n" + fromus_objectname + "\n\nImage: \n" + fromus_img + " \n\nPrix minimal: \n" + fromus_pricemin + " \n\nDescription: \n" + fromus_desc);	// Affichage des informations recuperees
+//window.alert("Vendeur: \n" + fromus_site + "\n\nOffre: \n" + fromus_offre + "\n\nNom: \n" + fromus_objectname + "\n\nImage: \n" + fromus_img + " \n\nPrix minimal: \n" + fromus_pricemin + " \n\nDescription: \n" + fromus_desc);	// Affichage des informations recuperees
