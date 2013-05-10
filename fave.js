023800cc88917b4324f12a554599269abb96eaaf
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-10 15:01:38 +0300

    Update fave.js

--- fave.js
+++ fave.js
@@ -465,9 +465,10 @@ switch(fromus_site)	//Permet de sélectionner le code relatif au site consulté
 				fromus_objectname				=	document.getElementById("pdp-title").innerText;		
 
 				fromus_pricemintmp				=	document.getElementsByClassName("pdp-single")[0].innerText;
-				fromus_pricemin					=	/(\$[0-9]{0,}\.[0-9]{2})/g.exec(fromus_pricemintmp)[0];				
+				fromus_pricemin					=	/(\$[0-9]{0,}\.[0-9]{2})/g.exec(fromus_pricemin)[0];				
 
-				fromus_imgtmp					=	document.getElementsByClassName("prod_main_img")[0].innerText;		
+				fromus_imgtmp					=	document.getElementsByClassName("prod_main_img")[0].innerHTML;
+				fromus_imgtmp					+=	'';				
 				fromus_img						= 	/(http\:\/\/.*)(\?\$pdp)/gi.exec(fromus_imgtmp)[0] + '';
 				fromus_img						=	fromus_img.substring(0,fromus_img.length-5);	
 
