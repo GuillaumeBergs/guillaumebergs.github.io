f896e00338376737c555a2ab735304614f36bfaf
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-14 10:37:05 +0200

    Update fave.js

--- fave.js
+++ fave.js
@@ -1784,7 +1784,7 @@ switch(fromus_site)	//Permet de sélectionner le code relatif au site consulté
 				fromus_img							= 	"non trouve";
 			}
 	}
-if(fromus_desc===undefined)//En cas d'absence de description, utiliser le nom du produit.
+if((fromus_desc===undefined) ||(fromus_desc==undefined)||(fromus_desc=='undefined'))//En cas d'absence de description, utiliser le nom du produit.
 		{
 			fromus_desc									=	fromus_objectname;
 		}
