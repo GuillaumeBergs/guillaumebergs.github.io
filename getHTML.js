83e622a451ba1558519386f1a1d7b1080616b070
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-14 17:39:07 +0300

    Update getHTML.js

--- getHTML.js
+++ getHTML.js
@@ -24,10 +24,10 @@ getSelectionHTML = function () {
     return '';
   }
 };
-var fromus_txt  = getSelectionHTML();
+var fromus_txt  = getSelectionHTML().replace(/\n/g,'');
 alert(fromus_txt);
 if(/id=\"/.test(fromus_txt))
-{alert(/id=\"[^\"]{1,}\"/g.exec(fromus_txt.replace(/\n/g,'')));}
+{alert(/id=\"[^\"]{1,}\"/g.exec(fromus_txt));}
 if(/class=\"/.test(fromus_txt))
 {alert(/class=\"[^\"]{1,}\"/g.exec(fromus_txt));}
 
