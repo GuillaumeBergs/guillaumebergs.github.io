e4f439bbb04b07804854292af38c81e2146e6188
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-16 15:33:45 +0300

    Update fave.js

--- fave.js
+++ fave.js
@@ -1212,8 +1212,7 @@ switch(fromus_site)	//Permet de sélectionner le code relatif au site consulté
 					
 				fromus_pricemin					=	document.getElementById("price-span").textContent;
 
-				fromus_imgtmp					=	document.getElementsByClassName("spp_image")[0].innerHTML;
-				fromus_img						=	'http://www.smashbox.com'+/\<img.*src=\"(.*(\.jpg|\.gif|\.png|\;)).*\>/.exec(fromus_imgtmp)[1];
+				fromus_imgtmp					=	document.getElementsByClassName("spp_image")[0].getElementsByTagName("img")[0].src;
 
 				if(document.getElementsByClassName("description")[0]!=undefined)
 					{
