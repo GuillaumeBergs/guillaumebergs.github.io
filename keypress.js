d379dc3feeef891593b32ca261a981c09bdbc320
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-21 12:42:29 +0300

    Update keypress.js

--- keypress.js
+++ keypress.js
@@ -1,3 +1,22 @@
+var bindEvent = function(elem, evt, cb) {
+    //vérifie si addEventListenerexiste dans l'élément
+    if (elem.addEventListener) {
+        elem.addEventListener(evt, cb, false);
+        //si addEventListener n'est pas présent, vérifie si le navigateur est une version d'IE
+    } else if (elem.attachEvent) {
+        //ajoute le préfixe "on" à l'event
+        elem.attachEvent('on' + evt, function() {
+            // Simule addEventListener ; s'assure que le callback obtient l'élément pour "this" et s'assure que le premier élément de la fonction est un event
+            cb.call(event.srcElement, event);
+        });
+    }
+}
+
+
+bindEvent(document, 'keypress', function(event) {
+    var target = event.target || event.srcElement;
+});
+
 function detectKey(Event) 
 { 
 
