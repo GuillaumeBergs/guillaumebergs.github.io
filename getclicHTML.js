9b54d2137b42bede0b97f59f48bbc30a19ca6382
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-21 10:22:38 +0200

    Update getclicHTML.js

--- getclicHTML.js
+++ getclicHTML.js
@@ -1,19 +1,19 @@
 var bindEvent = function(elem ,evt,cb) {
-        //vérifie si addEventListenerexiste dans l'élément
-        if ( elem.addEventListener ) {
-            elem.addEventListener(evt,cb,false);
+	//vérifie si addEventListenerexiste dans l'élément
+	if ( elem.addEventListener ) {
+		elem.addEventListener(evt,cb,false);
         //si addEventListener n'est pas présent, vérifie si le navigateur est une version  d'IE
         } else if ( elem.attachEvent ) {
-            //ajoute le préfixe "on" à l'event
-            elem.attachEvent('on' + evt, function(){
-                // Simule addEventListener ; s'assure que le callback obtient l'élément pour "this" et s'assure que le premier élément de la fonction est un event
-                 cb.call(event.srcElement,event);
-            });
-        }
-    }
+		//ajoute le préfixe "on" à l'event
+		elem.attachEvent('on' + evt, function(){
+			// Simule addEventListener ; s'assure que le callback obtient l'élément pour "this" et s'assure que le premier élément de la fonction est un event
+			cb.call(event.srcElement,event);
+		});
+	}
+}
 
 bindEvent(document,'click', function(event) 
 { var target = event.target || event.srcElement;
-        alert ( target.innerHTML );
-	 this.removeEventListener('click',arguments.callee,false);
+	alert ( target.innerHTML );
+	this.removeEventListener('click',arguments.callee,false);
 });
