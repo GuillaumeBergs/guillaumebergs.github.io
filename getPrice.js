ed593ae6e6a8f7af8dd89dea382d8660b8105e2d
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-31 09:36:41 +0200

    Update getPrice.js

--- getPrice.js
+++ getPrice.js
@@ -12,7 +12,7 @@ var bindEvent = function(elem ,evt,cb) {
 	}
 }
 
-bindEvent(document,'focusin', function(event) 
+bindEvent(document,'mouseover', function(event) 
 { var target = event.target || event.srcElement;
 	
 	var 	fromus_txt    = target.innerHTML;
