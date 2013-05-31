3c232925e684ea23376724fa995fd46bc0712cdf
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-05-31 10:57:39 +0300

    Update getPrice.js

--- getPrice.js
+++ getPrice.js
@@ -12,122 +12,115 @@ var bindEvent = function(elem ,evt,cb) {
 	}
 }
 
-bindEvent(elem,'mouseover', function(event) 
+bindEvent(document,'mouseover', function(event) 
 { var target = event.target || event.srcElement;
 	
+	console.log('mouseover');
 	
-	if (event.target == elem) {
-		// Mouse pointer entered elem
-		element = document.getElementById('elem');
-		elem.style.background = '#ff00aa';
-	}
 	
 }
 
 bindEvent(elem,'mouseout', function(event) 
 { var target = event.target || event.srcElement;
 	
-	elem.addEventListener("mouseout", function(event) {
-		if (event.target == elem) {
-			// Mouse pointer left elem
-			element = document.getElementById('elem');
-			elem.style.background = '#ff00aa';
-		}
-	}, false);
+	console.log('mouseout');
 	
 	
+}, false);
+
+
+
+
+bindEvent(document,'click', function(event) 
+{ var target = event.target || event.srcElement;
 	
+	var 	fromus_txt    = target.innerHTML;
+	var 	fromus_selectedText  = target.textContent;
+	var 	fromus_selectedTexttmp;
 	
-	bindEvent(document,'click', function(event) 
-	{ var target = event.target || event.srcElement;
-		
-		var 	fromus_txt    = target.innerHTML;
-		var 	fromus_selectedText  = target.textContent;
-		var 	fromus_selectedTexttmp;
-		
-		var 	fromus_site 	=	document.location.href;		//récupération de l'adresse fromus_
-		fromus_site 	=	/http[s]{0,1}\:\/\/(.*\.com)/gi.exec(fromus_site)[1];
-		fromus_site	=	/\.[a-z0-9\-A-Z]{1,}\.com$/.exec(fromus_site)[0];
-		fromus_site	=	'www'+fromus_site;
-		
-		fromus_txt        = fromus_txt.replace(/\n/g,'');
-		console.log(target.textContent);
-		
-		if(/id=\"/.test(fromus_txt))
+	var 	fromus_site 	=	document.location.href;		//récupération de l'adresse fromus_
+	fromus_site 	=	/http[s]{0,1}\:\/\/(.*\.com)/gi.exec(fromus_site)[1];
+	fromus_site	=	/\.[a-z0-9\-A-Z]{1,}\.com$/.exec(fromus_site)[0];
+	fromus_site	=	'www'+fromus_site;
+	
+	fromus_txt        = fromus_txt.replace(/\n/g,'');
+	console.log(target.textContent);
+	
+	if(/id=\"/.test(fromus_txt))
+	{
+		var fromus_idmatch    = fromus_txt.match(/id=(\"[^\"]{1,}\")/mgi);
+		console.log(fromus_idmatch);
+	}
+	if(/class=\"/.test(fromus_txt))
+	{
+		var fromus_classmatch = fromus_txt.match(/class=(\"[^\"]{1,}\")/mgi);  
+		console.log(fromus_classmatch);
+	}
+	
+	console.log("Ce qui est ajouté à la base de données est...");
+	
+	if(fromus_idmatch !=undefined)
+	{
+		if(fromus_classmatch !=undefined)
 		{
-			var fromus_idmatch    = fromus_txt.match(/id=(\"[^\"]{1,}\")/mgi);
-			console.log(fromus_idmatch);
-		}
-		if(/class=\"/.test(fromus_txt))
-		{
-			var fromus_classmatch = fromus_txt.match(/class=(\"[^\"]{1,}\")/mgi);  
-			console.log(fromus_classmatch);
-		}
-		
-		console.log("Ce qui est ajouté à la base de données est...");
-		
-		if(fromus_idmatch !=undefined)
-		{
-			if(fromus_classmatch !=undefined)
-			{
-				// id et class 
-				
-				fromus_selectedTexttmp	= fromus_idmatch[0].substring(4,fromus_idmatch[0].length-1);			
-				console.log('fromus_sitelist[\''+fromus_site+'\'].price_id.push(\''+fromus_selectedTexttmp+'\');');
-				
-				fromus_selectedTexttmp	=	fromus_classmatch[0].substring(7,fromus_classmatch[0].length-1);			
-				console.log('fromus_sitelist[\''+fromus_site+'\'].price_class.push(\''+fromus_selectedTexttmp+'\');');
-				
-			}
-			else
-			{
-				//id sans class
-				fromus_selectedTexttmp	=	fromus_idmatch[0].substring(4,fromus_idmatch[0].length-1);			
-				console.log('fromus_sitelist[\''+fromus_site+'\'].price_id.push(\''+fromus_selectedTexttmp+'\');');
-				
-			}
+			// id et class 
 			
-			fromus_selectedText	=	fromus_idmatch[0].substring(4,fromus_idmatch[0].length-1);
-			fromus_selectedText	=	document.getElementById(fromus_selectedText).textContent;
-			if(/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.test(fromus_selectedText))
-			{
-				fromus_selectedText	=	/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.exec(fromus_selectedText)[0];
-			}
+			fromus_selectedTexttmp	= fromus_idmatch[0].substring(4,fromus_idmatch[0].length-1);			
+			console.log('fromus_sitelist[\''+fromus_site+'\'].price_id.push(\''+fromus_selectedTexttmp+'\');');
+			
+			fromus_selectedTexttmp	=	fromus_classmatch[0].substring(7,fromus_classmatch[0].length-1);			
+			console.log('fromus_sitelist[\''+fromus_site+'\'].price_class.push(\''+fromus_selectedTexttmp+'\');');
 			
 		}
 		else
 		{
-			if(fromus_classmatch !=undefined)
-			{ //Class sans id
-				fromus_selectedTexttmp	=	fromus_classmatch[0].substring(7,fromus_classmatch[0].length-1);
-				console.log('fromus_sitelist[\''+fromus_site+'\'].price_class.push(\''+fromus_selectedTexttmp+'\');');
-				
-				fromus_selectedText = fromus_classmatch[0].substring(7,fromus_classmatch[0].length-1);
-				
-				fromus_selectedText	=	document.getElementsByClassName(fromus_selectedText)[0].textContent;
-				console.log(fromus_selectedText);
-				if(/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.test(fromus_selectedText))
-				{
-					fromus_selectedText	=	/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.exec(fromus_selectedText)[0];
-				}
-				else
-				{
-					fromus_selectedText	=	'';
-				}
+			//id sans class
+			fromus_selectedTexttmp	=	fromus_idmatch[0].substring(4,fromus_idmatch[0].length-1);			
+			console.log('fromus_sitelist[\''+fromus_site+'\'].price_id.push(\''+fromus_selectedTexttmp+'\');');
+			
+		}
+		
+		fromus_selectedText	=	fromus_idmatch[0].substring(4,fromus_idmatch[0].length-1);
+		fromus_selectedText	=	document.getElementById(fromus_selectedText).textContent;
+		if(/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.test(fromus_selectedText))
+		{
+			fromus_selectedText	=	/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.exec(fromus_selectedText)[0];
+		}
+		
+	}
+	else
+	{
+		if(fromus_classmatch !=undefined)
+		{ //Class sans id
+			fromus_selectedTexttmp	=	fromus_classmatch[0].substring(7,fromus_classmatch[0].length-1);
+			console.log('fromus_sitelist[\''+fromus_site+'\'].price_class.push(\''+fromus_selectedTexttmp+'\');');
+			
+			fromus_selectedText = fromus_classmatch[0].substring(7,fromus_classmatch[0].length-1);
+			
+			fromus_selectedText	=	document.getElementsByClassName(fromus_selectedText)[0].textContent;
+			console.log(fromus_selectedText);
+			if(/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.test(fromus_selectedText))
+			{
+				fromus_selectedText	=	/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.exec(fromus_selectedText)[0];
 			}
 			else
 			{
-				//ni class ni id
-				console.log('Rien.')
-				if(/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.test(fromus_selectedText))
-				{
-					fromus_selectedText	=	/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.exec(fromus_selectedText)[0];
-				}
-			}  
+				fromus_selectedText	=	'';
+			}
 		}
-		console.log("Et ce qui est affiché dans la case est...");
-		console.log(fromus_selectedText);
-		localStorage["regPrice"] = fromus_selectedText;
-		
-		this.removeEventListener('click',arguments.callee,false);
-	});	
+		else
+		{
+			//ni class ni id
+			console.log('Rien.')
+			if(/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.test(fromus_selectedText))
+			{
+				fromus_selectedText	=	/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.exec(fromus_selectedText)[0];
+			}
+		}  
+	}
+	console.log("Et ce qui est affiché dans la case est...");
+	console.log(fromus_selectedText);
+	localStorage["regPrice"] = fromus_selectedText;
+	
+	this.removeEventListener('click',arguments.callee,false);
+});	
