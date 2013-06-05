b13acf716d4c0b2cea09bc959b8026cf815ee4f7
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-06-05 15:31:35 +0300

    Update getPrice.js

--- getPrice.js
+++ getPrice.js
@@ -83,7 +83,7 @@ var inversHTML	=	function(htmlcode){
 
 if(fus_actprice == 0)
 {
-	var bindEvent(document,'mouseover', function(event) 
+	bindEvent(document,'mouseover', function(event) 
 	{ var target = event.target || event.srcElement;
 		if(fus_actprice == 1)	// Si on cherceh le prix...
 		{
@@ -99,7 +99,7 @@ if(fus_actprice == 0)
 }
 if(fus_actprice == 0)
 {
-	var bindEvent(document,'mouseout', function(event) 
+	bindEvent(document,'mouseout', function(event) 
 	{ var target = event.target || event.srcElement;
 		if(fus_actprice == 1)	// Si on cherceh le prix...
 		{
