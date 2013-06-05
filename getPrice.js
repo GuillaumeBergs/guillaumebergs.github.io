44945d7f3f85184b73e3327403f179ffc049d13f
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2013-06-05 16:45:38 +0300

    Update getPrice.js

--- getPrice.js
+++ getPrice.js
@@ -1,5 +1,3 @@
-var fus_actprice = 1; // V ariable indiquant que l'on est à la recherche du prix
-
 var bindEvent = function(elem ,evt,cb) {
 	//vérifie si addEventListenerexiste dans l'élément
 	if ( elem.addEventListener ) {
@@ -14,9 +12,13 @@ var bindEvent = function(elem ,evt,cb) {
 	}
 }
 
+
+
+
 var inversHTML	=	function(htmlcode){
 	console.log('start, htmlcode = ' + htmlcode);
-	
+
+
     if (htmlcode.substr(0, 1) === '#') 
 	{
 		htmlcode = htmlcode.substr(1,7);
@@ -27,109 +29,91 @@ var inversHTML	=	function(htmlcode){
 	else
 	{	
 		var digits = /(rgb[a]{0,1}\()(\d+), (\d+), (\d+)(.*)/.exec(htmlcode);
-		
+
 		if(/, \d+/.test(digits[5]))
 		{
 			var alpha = parseInt(/\d+/.exec(digits[5]));
-		}
-		
+			}
+
 		var red = parseInt(digits[2]);
 		var green = parseInt(digits[3]);
 		var blue = parseInt(digits[4]);
-		
+
 		console.log('ColorConvert,');
 		console.log('red = ' + red);
 		console.log('green = ' + green);
 		console.log('blue = ' + blue);
 		console.log('alpha = ' + alpha);
-		
+
+
 		red 		=	red		^	255;
 		green	=	green	^	255;
 		blue	=	blue	^	255;
-		
+
 		if(alpha!=undefined)
 		{
 			alpha	=	alpha	^	255;
-		}
-		
+			}
+
 		console.log('XOR' );
-		
+
 		console.log('red = ' + red);
 		console.log('green = ' + green);
 		console.log('blue = ' + blue);							
 		console.log('alpha = ' + alpha);
-		
+
 		red 	=	red.toString(10);
 		green	=	green.toString(10);
 		blue	=	blue.toString(10);
-		
+
 		if(alpha!=undefined)
 		{
 			alpha	=	alpha.toString(10);
-		}
-		
+			}
+
 		htmlcode = red+','+green+','+blue;
 		if(alpha!=undefined)
 		{
 			htmlcode = htmlcode + ',' + alpha;
 		}
 		console.log('end après toString, htmlcode = ' + htmlcode);
-		
+
 		htmlcode = digits[1]+htmlcode+'\)';
 		console.log('au return, htmlcode = ' + htmlcode);	
 		return htmlcode;
 	}
 }
 
-if(fus_actprice == 0)
-{
-	var mouser = bindEvent(document,'mouseover', function(event) 
-	{ var target = event.target || event.srcElement;
-		if(fus_actprice == 1)	// Si on cherceh le prix...
-		{
-			console.log('mouseover');
-			target.style.backgroundColor = inversHTML(getComputedStyle(target).backgroundColor);	
-			// !!WARNING!! getComputedStyle n'est pas compatible avec IE, utiliser currentStyle à la place !!WARNING!! //
-		}
-		else
-		{
-			this.removeEventListener('mouseover',arguments.callee,false);			
-		}
-	});
-}
-if(fus_actprice == 0)
-{
-	var mouset = bindEvent(document,'mouseout', function(event) 
-	{ var target = event.target || event.srcElement;
-		if(fus_actprice == 1)	// Si on cherceh le prix...
-		{
-			console.log('mouseout');
-			target.style.backgroundColor = inversHTML(getComputedStyle(target).backgroundColor);
-			// !!WARNING!! getComputedStyle n'est pas compatible avec IE, utiliser currentStyle à la place !!WARNING!! //
-		}
-		else
-		{
-			this.removeEventListener('mouseout',arguments.callee,false);			
-		}
-	});
-}
+var mouser = bindEvent(document,'mouseover', function(event) 
+{ var target = event.target || event.srcElement;
+	console.log('mouseover');
+	target.style.backgroundColor = inversHTML(getComputedStyle(target).backgroundColor);
+});
+
+var mouset = bindEvent(document,'mouseout', function(event) 
+{ var target = event.target || event.srcElement;
+	console.log('mouseout');
+	target.style.backgroundColor = inversHTML(getComputedStyle(target).backgroundColor);
+});
+
+
 
 
 bindEvent(document,'click', function(event) 
 { var target = event.target || event.srcElement;
-	
+
 	var 	fromus_txt    = target.innerHTML;
 	var 	fromus_selectedText  = target.textContent;
 	var 	fromus_selectedTexttmp;
-	
-	var 	fromus_site 	=	document.location.href;		//récupération de l'adresse fromus_ site
+
+	var 	fromus_site 	=	document.location.href;		//récupération de l'adresse fromus_
 	fromus_site 	=	/http[s]{0,1}\:\/\/(.*\.com)/gi.exec(fromus_site)[1];
 	fromus_site	=	/\.[a-z0-9\-A-Z]{1,}\.com$/.exec(fromus_site)[0];
 	fromus_site	=	'www'+fromus_site;
-	
+
 	fromus_txt        = fromus_txt.replace(/\n/g,'');
 	console.log(target.textContent);
-	
+
 	if(/id=\"/.test(fromus_txt))
 	{
 		var fromus_idmatch    = fromus_txt.match(/id=(\"[^\"]{1,}\")/mgi);
@@ -140,37 +124,37 @@ bindEvent(document,'click', function(event)
 		var fromus_classmatch = fromus_txt.match(/class=(\"[^\"]{1,}\")/mgi);  
 		console.log(fromus_classmatch);
 	}
-	
+
 	console.log("Ce qui est ajouté à la base de données est...");
-	
+
 	if(fromus_idmatch !=undefined)
 	{
 		if(fromus_classmatch !=undefined)
 		{
 			// id et class 
-			
+
 			fromus_selectedTexttmp	= fromus_idmatch[0].substring(4,fromus_idmatch[0].length-1);			
 			console.log('fromus_sitelist[\''+fromus_site+'\'].price_id.push(\''+fromus_selectedTexttmp+'\');');
-			
+
 			fromus_selectedTexttmp	=	fromus_classmatch[0].substring(7,fromus_classmatch[0].length-1);			
 			console.log('fromus_sitelist[\''+fromus_site+'\'].price_class.push(\''+fromus_selectedTexttmp+'\');');
-			
+
 		}
 		else
 		{
 			//id sans class
 			fromus_selectedTexttmp	=	fromus_idmatch[0].substring(4,fromus_idmatch[0].length-1);			
 			console.log('fromus_sitelist[\''+fromus_site+'\'].price_id.push(\''+fromus_selectedTexttmp+'\');');
-			
+
 		}
-		
+
 		fromus_selectedText	=	fromus_idmatch[0].substring(4,fromus_idmatch[0].length-1);
 		fromus_selectedText	=	document.getElementById(fromus_selectedText).textContent;
 		if(/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.test(fromus_selectedText))
 		{
 			fromus_selectedText	=	/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.exec(fromus_selectedText)[0];
 		}
-		
+
 	}
 	else
 	{
@@ -178,9 +162,9 @@ bindEvent(document,'click', function(event)
 		{ //Class sans id
 			fromus_selectedTexttmp	=	fromus_classmatch[0].substring(7,fromus_classmatch[0].length-1);
 			console.log('fromus_sitelist[\''+fromus_site+'\'].price_class.push(\''+fromus_selectedTexttmp+'\');');
-			
+
 			fromus_selectedText = fromus_classmatch[0].substring(7,fromus_classmatch[0].length-1);
-			
+
 			fromus_selectedText	=	document.getElementsByClassName(fromus_selectedText)[0].textContent;
 			console.log(fromus_selectedText);
 			if(/(\$[0-9\,]{0,}[\.0-9]{0,3})/g.test(fromus_selectedText))
@@ -205,7 +189,7 @@ bindEvent(document,'click', function(event)
 	console.log("Et ce qui est affiché dans la case est...");
 	console.log(fromus_selectedText);
 	localStorage["regPrice"] = fromus_selectedText;
-	fus_actprice = 0;	// On ne cherche plus le prix
-	target.style.backgroundColor = inversHTML(getComputedStyle(target).backgroundColor);	
+	mouser.removeEventListener('mouseover',arguments.calle,false);
+	mouset.removeEventListener('mouseout',arguments.calle,false);
 	this.removeEventListener('click',arguments.callee,false);
 });		
